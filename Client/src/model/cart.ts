5f2a7d9ec0eef66d8e39c3ee1cb44d0df191b1ca
morant2 <123651369+morant2@users.noreply.github.com>
2023-03-07 13:13:47 -0500

    Create cart.ts

---
+++ Client/src/model/cart.ts
@@ -0,0 +1,14 @@
+import { ref } from "vue";
+import type { Product } from "./products";
+
+const cart = ref([] as CartItem[]);
+
+export function useCart() {
+    return cart;
+}
+
+export interface CartItem {
+    productId: number;
+    product: Product;
+    quantity: number;
+}
